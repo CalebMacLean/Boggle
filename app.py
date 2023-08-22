f4ed854109d03a81f6ca3c4b5be3a0380f56db7d
CalebMacLean <CMaclean98@gmail.com>
2023-08-22 14:17:24 -0700

    Created Base Template, Home Route, and Board

--- app.py
+++ app.py
@@ -1,3 +1,29 @@
+# Imports
 from boggle import Boggle
+from flask import Flask, render_template, redirect, request, session, flash 
+from flask_debugtoolbar import DebugToolbarExtension
 
+# Application Configurations and Global Features
 boggle_game = Boggle()
+app = Flask(__name__)
+
+CURRENT_BOARD_KEY = 'current_board'
+
+app.config['SECRET_KEY'] = 'CanY0uGue551t'
+app.config['TESTING'] = True
+# app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
+
+app.debug = True
+toolbar = DebugToolbarExtension(app)
+
+# Application
+# Home Page
+@app.route('/')
+def show_board():
+    """Loads Boggle Board and Saves it to Session"""
+    board = boggle_game.make_board()
+    session[CURRENT_BOARD_KEY] = board
+
+   
+    return render_template('start-game.html',
+                           board = board)
